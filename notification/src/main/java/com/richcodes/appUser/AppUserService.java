5e82a4adad96def37a1e0fa4962fd47b8379f429
Richswag009 <richesmetelewawon@gmail.com>
2024-03-18 23:25:55 +0100

    addded user repository

--- notification/src/main/java/com/richcodes/appUser/AppUserService.java
+++ notification/src/main/java/com/richcodes/appUser/AppUserService.java
@@ -1,2 +1,29 @@
-package com.richcodes.appUser;public class AppUserService {
+package com.richcodes.appUser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService  implements UserDetailsService {
+    public static final String message =
+            "user with email %S not found";
+
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email).orElseThrow(
+                ()->new UsernameNotFoundException(String.format(message,email))
+        );
+    }
 }
